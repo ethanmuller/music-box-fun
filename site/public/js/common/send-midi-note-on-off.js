19addaed68c5121438c8a972b5c6ce2588acf99a
Ethan Muller <ethanbmuller@gmail.com>
2024-08-13 17:08:15 -0400

    pc update

--- site/public/js/common/send-midi-note-on-off.js
+++ site/public/js/common/send-midi-note-on-off.js
@@ -1,13 +1,16 @@
 import { musicBoxStore } from '../music-box-store.js';
 
   export function sendMidiNoteOnOff(midiNoteNumber, time) {
-    const noteOnMessage = [0x90, midiNoteNumber, 0x7f];
+    const noteOnMessage =  [0x90, midiNoteNumber, 0x7f];
+    const noteOffMessage = [0x80, midiNoteNumber, 0x7f];
     const portID = musicBoxStore.state.appState.midiOutputId;
     const midiAccess = musicBoxStore.state.appState.midiAccess
     if (!midiAccess) return;
     const output = midiAccess.outputs.get(portID);
     if (!output) return;
-    output.send(noteOnMessage, 5000);
-    const noteOffMessage = [0x80, midiNoteNumber, 0x7f];
-    output.send(noteOffMessage, 6000);
+
+    const syncDelay = 700
+
+    output.send(noteOnMessage);
+    window.setTimeout(() => {output.send(noteOffMessage)}, 1000);
   }
